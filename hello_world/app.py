e73180eba6bc42f0139665cd2006dd9bce1e20a7
Sean Connolly <connolly.st@gmail.com>
2021-09-08 19:21:08 -1000

    Renamed hello world app

--- hello_world/app.py
+++ hello_world/app.py
@@ -27,7 +27,7 @@ def lambda_handler(event, context):
         "statusCode": 200,
         "body": json.dumps(
             {
-                "message": "hello world",
+                "message": "hello sean",
             }
         ),
     }
